d0f6aa504eef4f2a86cca18d28745fd476f47e0b
Chianna Cohen <chianna@fb.com>
2020-07-17 02:06:07 -0700

    submit photo, message item xml, main activitiy xml

--- app/src/main/java/com/example/livingtogether/ComposeActivity.java
+++ app/src/main/java/com/example/livingtogether/ComposeActivity.java
@@ -1,9 +1,12 @@
 package com.example.livingtogether;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.net.Uri;
@@ -20,9 +23,14 @@ import android.widget.Toast;
 import com.example.livingtogether.models.CustomUser;
 import com.example.livingtogether.models.Message;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.SaveCallback;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class ComposeActivity extends AppCompatActivity implements View.OnClickListener {
@@ -78,7 +86,7 @@ public class ComposeActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     // opens new activity - camera
-    private void launchCamera() {
+    public void launchCamera() {
         // create Intent to take a picture and return control to the calling application
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Create a File reference for future access
@@ -98,6 +106,7 @@ public class ComposeActivity extends AppCompatActivity implements View.OnClickLi
         }
     }
 
+    // Returns the File for a photo stored on disk given the fileName
     public File getPhotoFileUri(String fileName) {
         // Get safe storage directory for photos
         // Use `getExternalFilesDir` on Context to access package-specific directories.
@@ -105,14 +114,65 @@ public class ComposeActivity extends AppCompatActivity implements View.OnClickLi
         File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
 
         // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
             Log.d(TAG, "failed to create directory");
         }
 
         // Return the file target for the photo based on filename
-        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                // RESIZE BITMAP
+                Uri takenPhotoUri = Uri.fromFile(getPhotoFileUri(photoFileName));
+                // by this point we have the camera photo on disk
+                Bitmap rawTakenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+                // See BitmapScaler.java: https://gist.github.com/nesquena/3885707fd3773c09f1bb
+                Bitmap resizedBitmap = scaleToFitWidth(rawTakenImage, 1500);
+
+                //resizedBitmap = rotateBitmap(resizedBitmap, 90);
+
+                // Configure byte output stream
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                // Compress the image further
+                resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+                // Create a new file for the resized bitmap (`getPhotoFileUri` defined above)
+                File resizedFile = getPhotoFileUri("resized_" + photoFileName);
+                try {
+                    resizedFile.createNewFile();
+                    FileOutputStream fos = new FileOutputStream(resizedFile);
+                    // Write the bytes of the bitmap to file
+                    fos.write(bytes.toByteArray());
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                photoFile = resizedFile;
+
+                ivPreview.setImageBitmap(resizedBitmap);
+                ivPreview.setVisibility(View.VISIBLE);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public static Bitmap scaleToFitWidth(Bitmap b, int width)
+    {
+        float factor = width / (float) b.getWidth();
+        return Bitmap.createScaledBitmap(b, width, (int) (b.getHeight() * factor), true);
+    }
     // saves message to database
     private void submit() {
         String title = etTitle.getText().toString();
@@ -129,7 +189,8 @@ public class ComposeActivity extends AppCompatActivity implements View.OnClickLi
             Log.i(TAG, curUser.getName());
             message.setCustomUser(curUser);
             message.setType(ANNOUNCEMENT_TYPE);
-            //message.setImage();
+            if (preview != null)
+                message.setImage(new ParseFile(photoFile));
             message.saveInBackground(new SaveCallback() {
                 @Override
                 public void done(ParseException e) {
@@ -140,6 +201,8 @@ public class ComposeActivity extends AppCompatActivity implements View.OnClickLi
                         Log.i(TAG, "Post saved!");
                         etBody.setText("");
                         etTitle.setText("");
+                        ivPreview.setImageResource(0);
+                        ivPreview.setVisibility(View.GONE);
                     }
                 }
             });
