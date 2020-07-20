91c06acc38e97fb63aedc3e39dbd1262e47f5a32
Chianna Cohen <chianna@fb.com>
2020-07-20 12:57:45 -0700

    add display name activity

---
+++ app/src/main/java/com/livingtogether/activites/CreateProfileActivity.java
@@ -0,0 +1,32 @@
+package com.livingtogether.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.livingtogether.livingtogether.R;
+
+public class CreateProfileActivity extends AppCompatActivity {
+FloatingActionButton fab;
+EditText etName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile);
+
+        fab = findViewById(R.id.fab);
+        etName = findViewById(R.id.etName);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = etName.getText().toString();
+                I
+            }
+        });
+
+    }
+}
